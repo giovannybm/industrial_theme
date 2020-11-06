74f032ba55ed74b92314edf51a61ee7a5c5318b9
Lexetam <giovannybm@gmail.com>
2020-11-05 23:15:53 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [
+      "./content/**/*.htm",
+      "./layouts/**/*.htm",
+      "./pages/**/*.htm",
+      "./partials/**/*.htm",
+
+    ],
+  theme: {
+    extend: {
+        colors: {
+        "blue-industrial-1": "#0d1929",
+        "blue-industrial-2": "#11304c",
+      },
+    height: theme => ({
+        "screen-p-70": "calc(100vh + 70px)",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      }),
+    },
+  },
+  variants: {
+    mixBlendMode: ['responsive'],
+    backgroundBlendMode: ['responsive'],
+    isolation: ['responsive'],
+  },
+  plugins: [
+      require('tailwindcss-blend-mode')(), // no options to configure
+  ],
+};
